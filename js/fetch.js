2f9ec409f80259d67677d8918de785a0742d6cdb
Tonywilson1211 <ant.wilson@hotmail.com>
2023-04-11 11:44:23 +0000

    fetch

---
+++ js/fetch.js
@@ -0,0 +1,2 @@
+// allows web requests to a server and manage what comes back
+// they are based on a 'promise' = 
---
+++ js/json.js
@@ -0,0 +1,23 @@
+/*
+* node json.js
+*
+*/
+
+
+// This is an object, made so by the curly braces. methods (functions) can be entered
+let bookCharacter = {
+    name: "Anthony",
+    employer: "Niche",
+    staff: "30",
+    dailyEmails: "150"
+}
+
+// This is an object within json - same format but only able to transfer info. no methods 
+[{
+    "name":"Anthony",
+    "employer": "Niche",
+    "staff": 30,
+    "dailyEmails": 150
+}]
+
+
